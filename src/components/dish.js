53e6f0ac79535b65fd0c637d46c3c6a0ed6ce836
Claudia <claudia.villarroel@live.com>
2021-05-11 22:46:55 -0400

    ADD: Importacion de estilos

--- src/components/dish.js
+++ src/components/dish.js
@@ -1,30 +1,38 @@
 import React, { Component, Fragment } from 'react';
 
-export class Flag extends Component{
-    render(){
-        return(
-            <Fragment>
-                <h1>Bandera</h1>
-                {/* <h1>Bandera2</h1> */}
-            </Fragment>
-        )
-    }
+
+export class Flag extends Component {
+  render() {
+    return (
+      <Fragment>
+        <h1>Bandera</h1>
+        {/* <h1>Bandera2</h1> */}
+      </Fragment>
+    );
+  }
 }
-export class Ingredient extends Component{
-    render(){
-        return React.createElement('h4',{}, "Ingredientes")
-    }
+export class Ingredient extends Component {
+  render() {
+    return (
+      <Fragment>
+        <h4>Ingredientes</h4>
+        <h4>Ingredientes2</h4>
+      </Fragment>
+    );
+
+    //return React.createElement('h4',{}, "Ingredientes")
+  }
 }
 
-class Dish extends Component{
-    render(){
-        return(
-            <div>
-                <h1>Platillos</h1>
-                <Ingredient/>
-            </div>
-        )
-    }
+class Dish extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Platillos</h1>
+        <Ingredient />
+      </div>
+    );
+  }
 }
 
 export default Dish;
