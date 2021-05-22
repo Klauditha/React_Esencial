d27574235be42b3a509f67652de8b0155cdf466b
Claudia <claudia.villarroel@live.com>
2021-05-21 22:22:14 -0400

    ADD: Navegacion con router

--- src/App.js
+++ src/App.js
@@ -4,24 +4,31 @@ import Header from './components/header';
 import Dish, * as D from './components/dish';
 import NewDish from './components/newDish';
 import { Component } from 'react';
+import Button from '@material-ui/core/Button';
 
 
 class App extends Component{
   dish = "tacos";
   dishes = ["tacos", "ceviche","paella"];
+  showDishes = e => {
+    e.preventDefault();
+    this.props.history.push('/platillos');
+  };
   render(){
   return (
     <div className="App">
      <Header/>
      <NewDish />
-					<Dish name={this.dish} qty="3"/>
+     <Button variant="contained"
+     color="secondary" onClick={this.showDishes}>Elegir</Button>
+				{/* 	<Dish name={this.dish} qty="3"/>
           yo como {this.dish}
           <ul>
             {
               this.dishes.map(dish=><li>{dish}</li>)
             }
           </ul>
-					<D.Flag/>
+					<D.Flag/> */}
     </div>
 
   );
---
+++ src/components/countries.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class countries extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Paises</h1>
+            </div>
+        );
+    }
+}
+
+export default countries;
